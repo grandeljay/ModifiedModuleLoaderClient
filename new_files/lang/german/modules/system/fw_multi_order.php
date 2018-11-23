68d425d6bbfd3fbbfe5b1078ec0c8599bba7af1f
Robin Wieschendorf <mail@robinwieschendorf.de>
2018-11-23 09:00:16 +0100

    Auf Modified 2.0.3.0 angepasst.

--- new_files/lang/german/modules/system/fw_multi_order.php
+++ new_files/lang/german/modules/system/fw_multi_order.php
@@ -1,5 +1,5 @@
 <?php
 define('MODULE_FW_MULTI_ORDER_TITLE', 'Massenverarbeitung (First-Web)');
-define('MODULE_FW_MULTI_ORDER_LONG_DESCRIPTION', 'Mit diesem Modul von First-Web können Sie bei allen ausgewählten Bestellungen gleichzeitig den Status ändern oder die Rechnungen drucken lassen.');
+define('MODULE_FW_MULTI_ORDER_LONG_DESCRIPTION', 'Mit diesem Modul von First-Web können Sie bei allen ausgewählten Bestellungen gleichzeitig den Status ändern.');
 define('MODULE_FW_MULTI_ORDER_STATUS_TITLE', 'Massenverarbeitung (First-Web) Module aktivieren?');
 define('MODULE_FW_MULTI_ORDER_STATUS_DESC', '');
