c48f85231587d06e6d34814173de82a2cfbaefa7
Robin Wieschendorf <mail@robinwieschendorf.de>
2019-11-05 09:57:42 +0100

    fix: Fehlermeldungen deaktiviert.

--- new_files/admin/fw_multi_order.php
+++ new_files/admin/fw_multi_order.php
@@ -8,15 +8,11 @@ require_once (DIR_FS_INC . 'xtc_format_price_order.inc.php');
 require_once (DIR_FS_INC . 'xtc_get_attributes_model.inc.php');
 require DIR_WS_CLASSES . 'order.php';
 
-//require_once (DIR_FS_INC . 'xtc_utf8_decode.inc.php');
-//require_once DIR_FS_DOCUMENT_ROOT . 'admin/fw_pdf_bill.php';
-//require_once DIR_FS_CATALOG . 'includes/classes/FPDF/PdfRechnung.php';
-
-restore_error_handler();
-restore_exception_handler();
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL ^ E_NOTICE);
+// restore_error_handler();
+// restore_exception_handler();
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL ^ E_NOTICE);
 
 $controller = new Controller();
 $controller->invoke();
