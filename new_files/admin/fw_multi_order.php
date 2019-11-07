b1a8b4137bfb7ec7103f32620a7c85a9d21688db
Robin Wieschendorf <mail@robinwieschendorf.de>
2019-11-07 09:59:18 +0100

    fix: Das Modul wird jetzt nicht mehr geladen, wenn der Status auf false gestellt wird.

--- new_files/admin/fw_multi_order.php
+++ new_files/admin/fw_multi_order.php
@@ -2,6 +2,11 @@
 use FirstWeb\MultiOrder\Classes\Controller;
 
 require ('includes/application_top.php');
+
+if (!defined('MODULE_FW_MULTI_ORDER_STATUS') || MODULE_FW_MULTI_ORDER_STATUS != 'true') {
+    return;
+}
+
 require_once DIR_FS_DOCUMENT_ROOT . '/vendor-no-composer/firstweb/MultiOrder/autoload.php';
 require_once (DIR_FS_INC . 'get_order_total.inc.php');
 require_once (DIR_FS_INC . 'xtc_format_price_order.inc.php');
